49a7aced0f3e90a9b2488a609abeac8950643744
thangarajk <thankarajbecse@gmail.com>
2019-10-19 15:35:20 +0530

    ada

---
+++ Test/src/com/basic/VariableDec.java
@@ -0,0 +1,25 @@
+package com.basic;
+
+public class VariableDec {
+	public static void main(String[] args) {
+		
+		byte b=12;
+		
+		short s=23;
+		
+		int i=56;
+		
+		long l=3434;
+		
+		float f =3.14f;
+		
+		double d=3.232;
+		
+		char c='A';
+		
+		boolean res = false;
+		
+		System.out.println(res);
+	}
+
+}
