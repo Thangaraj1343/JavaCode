e5c0e1f4865ed04d4a2b17e6921fba6d6c50db09
thangarajk <thankarajbecse@gmail.com>
2019-10-26 15:15:01 +0530

    if stmt

---
+++ Test/src/com/basic/If_Statement.java
@@ -0,0 +1,15 @@
+package com.basic;
+
+public class If_Statement {
+	public static void main(String[] args) {
+		
+		int age =12;
+		if(age>18)
+		{
+			System.out.println("In if statement");
+		}
+		
+		System.out.println("done...");
+	}
+
+}
