358f3e86940e25aac63d46565f0ce026422e033f
thangarajk <thankarajbecse@gmail.com>
2019-10-26 15:23:52 +0530

    asdfsd

---
+++ Test/src/com/basic/If_Else_Statement.java
@@ -0,0 +1,17 @@
+package com.basic;
+
+public class If_Else_Statement {
+	public static void main(String[] args) {
+		
+		int age=18;
+		if(age>18)
+		{
+			System.out.println("Welcome");
+		}
+		else
+		{
+			System.out.println("Try again");
+		}
+	}
+
+}
